0c7e9d02d42830976337c81ed86683c060fb9ed3
Joseph480 <josephhart48@yahoo.com>
2021-03-10 20:29:08 -0500

    Gun Fixes
    Gun will pull back to keep from shooting through walls.

---
+++ Damascus Blue (2.0)/Assets/Scripts/Player Code/PullBack_Ray.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullBack_Ray : MonoBehaviour
+{
+    public float Length;
+    public GameObject Holster, Gun;
+
+    public bool Hitting;
+
+    private Ray Ray;
+    private RaycastHit Hit = new RaycastHit();
+    private Vector3 Origin, Offset;
+
+    void Start(){
+        Origin = Holster.transform.localPosition;
+        Offset = new Vector3 (Origin.x,Origin.y,Origin.z - 1.5f);
+    }
+    void Update(){
+        CastRay();
+        PositionGun();
+    }
+    void CastRay(){
+        Ray = new Ray(transform.position,transform.forward);
+        Debug.DrawRay(transform.position,transform.forward * Length, Color.red);
+        if (Physics.Raycast(Ray, out Hit, Length)){
+            Hitting = true;
+        } else Hitting = false;
+        
+    }
+    void PositionGun(){
+        if (Hitting)Holster.transform.localPosition = Vector3.Lerp(Holster.transform.localPosition, Offset, 0.02f);
+        else Holster.transform.localPosition = Vector3.Lerp (Holster.transform.localPosition, Origin, 0.02f);
+    }
+}

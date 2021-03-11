a240834fac6a039d9301ce514f1bac4e396f3337
Joseph480 <josephhart48@yahoo.com>
2021-03-11 03:59:28 -0500

    Wood texture. Non functioning Action Pad.
    Action pad does not doing anything yet, but it has a collider.

--- Damascus Blue (2.0)/Assets/Scripts/Damage_Check.cs
+++ Damascus Blue (2.0)/Assets/Scripts/Damage_Check.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class Damage_Check : MonoBehaviour
 {
-    public bool GivesFerocity, Breaks;
-    public int Health;
+    public bool GivesFerocity, Breaks, DropLoot, DropRandomLoot;
+    public int Health, LootDropIndex;
+    public GameObject [] Loot;
+
+    private Vector3 OffSet =  new Vector3 (0,0,0.2f);
 
     void OnTriggerEnter(Collider other){
         if (other.gameObject.tag == "Projectile"){
             if (GivesFerocity){Player_Stats.Ferocity++; 
             Player_Stats.Ferocity = Mathf.Clamp(Player_Stats.Ferocity, 0, 200);}
-            if (Breaks) {Health = Health - (int)Player_Stats.Ferocity/4; if (Health <= 0) Destroy(this.gameObject);}
+            if (Breaks) {Health = Health - (int)Player_Stats.Ferocity/4; if (Health <= 0) Destruct();}
         }
+    }
+    void Destruct(){
+        if (DropLoot){
+            Instantiate(Loot[LootDropIndex], transform.position, Quaternion.identity);
+        }
+        if (DropRandomLoot){
+            int x = Random.Range(0,Loot.Length);
+            Instantiate(Loot[x], transform.position, Quaternion.identity);
+        }
+        Destroy(this.gameObject);
     }
 }

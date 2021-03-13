cedfbe32c0bf1317cd64f78955c9a111c4440853
Joseph480 <josephhart48@yahoo.com>
2021-03-12 23:03:19 -0500

    Lots of Edits to A.I script
    This is gonna be a long one.

--- Damascus Blue (2.0)/Assets/Scripts/AI/AI_Navigation.cs
+++ Damascus Blue (2.0)/Assets/Scripts/AI/AI_Navigation.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class AI_Navigation : MonoBehaviour
 {
     public float MoveSpeed, LookSpeedChase, LookSpeedIdel, IdelPathTime, SightLength, Alertness;
-    public bool Idel, HasPath; // Send idel to private after tests.
+    public bool HasPath;
     //Create points for path. Draw line during editor time from [i] to [i++] foreach();
     public Vector3[] Path;
 
@@ -14,45 +14,41 @@ public class AI_Navigation : MonoBehaviour
     private Quaternion IdelDir, TargetDir;
     private Player_Control Player;
     private float PlayerDistance;
+    private bool Sighting, EmptyRay, Idel;
     private Rigidbody RB;
     private Ray Vision;
     private RaycastHit Hit; 
-    private IEnumerator IdelRoutine, AlertRoutine;
+    private IEnumerator IdelRoutine, AlertRoutine, RandomRoutine;
 
     void Start(){
         Player = FindObjectOfType<Player_Control>();
         RB = GetComponent<Rigidbody>();
         Origin = transform.position;
         RayOriginOffset = new Vector3(0,0.5f,0);
-        AlertRoutine = Lingering();
-        IdelRoutine = NewDirection(); 
-        StartCoroutine(IdelRoutine);
-        //Reset as coroutine.
-        RandomDetect();
+        if (!HasPath){IdelRoutine = NewDirection(); StartCoroutine(IdelRoutine);}
+        AlertRoutine = Lingering(); StartCoroutine(AlertRoutine);
+        RandomRoutine = RandomDetect(); StartCoroutine(RandomRoutine);
         Idel = true;
     }
-    void FixedUpdate()
-    {
+    void Update(){
         Detect();
+    }
+    void FixedUpdate(){
         if (HasPath){
             //FollowPath.
-
             //Never Walk idel so end with return.
             return;
         }
         if (Idel) WalkIdel(); else Follow(); 
     }
-    void OnTriggerEnter (Collider other)
-    {
-        switch (other.tag)
-        {
+    void OnTriggerEnter (Collider other){
+        switch (other.tag){
             //add tag name for projectiles, so enemy gets alert upon damage.
             case "": Idel = false; break;
             default: break;
         }
     }
-    void Detect()
-    {
+    void Detect(){
         Vision.origin = transform.position + RayOriginOffset;
         if (Idel) Vision.direction = transform.forward; 
         else {
@@ -62,21 +58,24 @@ public class AI_Navigation : MonoBehaviour
         }
         if (Physics.Raycast(Vision, out Hit, SightLength))
         {
-            Debug.DrawRay(Vision.origin, Vision.direction * SightLength, Color.red);
-            if (Hit.collider.tag == "Player") Idel = false; StopCoroutine(IdelRoutine);
-            if (!Idel && Hit.collider.tag != "Player") Idel = true; StartCoroutine(AlertRoutine);
-        }
+            if (Hit.collider.tag == "Player"){ 
+                Idel = false; Sighting = true;
+            }
+            if (!Idel && (Hit.collider.tag != "Player" || EmptyRay)){ 
+                Sighting = false;
+            }
+            EmptyRay = false;
+        } else EmptyRay = true;
     }
-    void Follow()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, MoveSpeed/50);
+    void Follow(){
+        RB.MovePosition(Vector3.MoveTowards(transform.position, Player.transform.position, MoveSpeed/50));
+        //transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, MoveSpeed/50);
         Target = new Vector3 (Player.transform.position.x, transform.position.y, Player.transform.position.z);
         TargetDir = Quaternion.LookRotation(Target - transform.position);
         transform.rotation = Quaternion.Slerp(transform.rotation, TargetDir, LookSpeedChase * Time.deltaTime);
     }
-    void WalkIdel()
-    {
-        transform.position += transform.forward * (MoveSpeed/100);
+    void WalkIdel(){
+        RB.MovePosition(Vector3.Lerp(transform.position, transform.position + transform.forward,(MoveSpeed/100)));
         transform.rotation = Quaternion.Slerp(transform.rotation, IdelDir, LookSpeedIdel * Time.deltaTime);
     }
     IEnumerator NewDirection(){
@@ -86,20 +85,24 @@ public class AI_Navigation : MonoBehaviour
     }
     IEnumerator Lingering(){
         yield return new WaitForSeconds(IdelPathTime * IdelPathTime);
-        if (Idel){
-            if (HasPath){/* Send monster back to following path*/} 
-            else{IdelRoutine = NewDirection(); StartCoroutine(IdelRoutine);}
-            StopCoroutine(AlertRoutine);
-        } 
-        else {AlertRoutine = Lingering(); StartCoroutine(AlertRoutine);}
+        if (!Sighting && !Idel) Idel = true;
+        AlertRoutine = Lingering(); StartCoroutine(AlertRoutine);
     }
-    //Change to IEnumerator and use IDelPathTime (as a float) divided by 2
-    void RandomDetect()
-    {
+    IEnumerator RandomDetect(){
         float i = 0;
         PlayerDistance = Vector3.Distance(Player.transform.position, transform.position);
-        i = Random.Range(0f, (float)PlayerDistance/Alertness);
-        if (i < 0.3f) {Idel = false;}
-        Invoke("RandomDetect", (float)IdelPathTime/2);
+        i = Random.Range(0f, (float)PlayerDistance/Alertness); if (i < 0.3f) Idel = false;
+        yield return new WaitForSeconds((float)IdelPathTime/2);
+        RandomRoutine = RandomDetect(); StartCoroutine(RandomRoutine);
     }
 }
+
+/*      NOTE SECTION
+
+Okay, use 4 or 8 rays at base of A.I; if forward ray is hitting something that is not the player. Then the 
+A.I should start moving in the direction of ray that is both not hitting something and whose length
+is closest to player.
+
+Maybe alertness should rely on dedicated ray?
+
+*/

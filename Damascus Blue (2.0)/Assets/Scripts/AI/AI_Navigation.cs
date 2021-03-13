de0de4a4e3cfd1408fed1958b8ee3fe472412b57
Joseph480 <josephhart48@yahoo.com>
2021-03-13 15:57:50 -0500

    A.I Updates
    Began navigation

--- Damascus Blue (2.0)/Assets/Scripts/AI/AI_Navigation.cs
+++ Damascus Blue (2.0)/Assets/Scripts/AI/AI_Navigation.cs
@@ -7,8 +7,10 @@ public class AI_Navigation : MonoBehaviour
 {
     public float MoveSpeed, LookSpeedChase, LookSpeedIdel, IdelPathTime, SightLength, Alertness;
     public bool HasPath;
+
     //Create points for path. Draw line during editor time from [i] to [i++] foreach();
-    public Vector3[] Path;
+    //public Vector3[] Path;
+    //public Vector3 NextNode;
 
     private Vector3 Origin, RayOriginOffset, RayAlertOffset, Target;
     private Quaternion IdelDir, CurrentDir, TargetDir;
@@ -18,16 +20,25 @@ public class AI_Navigation : MonoBehaviour
     private Rigidbody RB;
     private Ray Vision;
     private RaycastHit Hit; 
-    private IEnumerator IdelRoutine, AlertRoutine, RandomRoutine;
+
+    //navigation variables
+    public float NavLength;
+    private Ray N, NE, E, SE, S, SW, W, NW;
+    private RaycastHit HitN, HitNE, HitE, HitSE, HitS, HitSW, HitW, HitNW;
+    private Vector3 PosN, PosNE, PosE, PosSE, PosS, PosSW, PosW, PosNW;
+    private Vector3 DiagonalOffset;
 
     void Start(){
         Player = FindObjectOfType<Player_Control>();
         RB = GetComponent<Rigidbody>();
         Origin = transform.position;
         RayOriginOffset = new Vector3(0,0.5f,0);
-        if (!HasPath){IdelRoutine = NewDirection(); StartCoroutine(IdelRoutine);}
-        AlertRoutine = Lingering(); StartCoroutine(AlertRoutine);
-        RandomRoutine = RandomDetect(); StartCoroutine(RandomRoutine);
+        DiagonalOffset = new Vector3(0,45,0);
+        //Cycles below
+        if (!HasPath)InvokeRepeating("NewDirection",IdelPathTime,IdelPathTime);
+        InvokeRepeating("Lingering", IdelPathTime*IdelPathTime,IdelPathTime*IdelPathTime);
+        InvokeRepeating("RandomDetect",(float)IdelPathTime/2,(float)IdelPathTime/2);
+        InvokeRepeating("Navigate",0.1f,0.1f);
         Idel = true;
     }
     void Update(){
@@ -35,8 +46,8 @@ public class AI_Navigation : MonoBehaviour
     }
     void FixedUpdate(){
         if (HasPath){
-            //FollowPath.
             if (!Idel) Follow();
+            // else FollowPath();
             //Never Walk idel so end with return.
             return;
         }
@@ -44,8 +55,7 @@ public class AI_Navigation : MonoBehaviour
     }
     void OnTriggerEnter (Collider other){
         switch (other.tag){
-            //add tag name for projectiles, so enemy gets alert upon damage.
-            case "": Idel = false; break;
+            case "Projectile": Idel = false; break;
             default: break;
         }
     }
@@ -55,20 +65,67 @@ public class AI_Navigation : MonoBehaviour
         else {
             RayAlertOffset = Player.transform.position - transform.position;
             RayAlertOffset.x = transform.forward.x; RayAlertOffset.z = transform.forward.z;
-            Vision.direction = RayAlertOffset;
+            Vision.direction = transform.forward + RayAlertOffset;
         }
         if (Physics.Raycast(Vision, out Hit, SightLength))
         {
             if (Hit.collider.tag == "Player"){ 
                 Idel = false; Sighting = true;
             }
-            if (!Idel && (Hit.collider.tag != "Player" || EmptyRay)){ 
+            if (!Idel && (Hit.collider.tag != "Player" || EmptyRay)){
                 Sighting = false;
             }
             EmptyRay = false;
         } else EmptyRay = true;
     }
+    void Navigate(){
+        N.direction = transform.forward;
+        NE.direction = transform.forward + DiagonalOffset;
+        E.direction = transform.right;
+        SE.direction = transform.right + DiagonalOffset;
+        S.direction = -transform.forward;
+        SW.direction = -transform.forward + DiagonalOffset;
+        W.direction = -transform.right;
+        NW.direction = -transform.right + DiagonalOffset;
+
+        Debug.DrawRay(Origin,N.direction * NavLength,Color.red);
+        Debug.DrawRay(Origin,NE.direction * NavLength,Color.red);
+        Debug.DrawRay(Origin,E.direction * NavLength,Color.red);
+        Debug.DrawRay(Origin,SE.direction * NavLength,Color.red);
+        Debug.DrawRay(Origin,S.direction * NavLength,Color.red);
+        Debug.DrawRay(Origin,SW.direction * NavLength,Color.red);
+        Debug.DrawRay(Origin,W.direction * NavLength,Color.red);
+        Debug.DrawRay(Origin,NW.direction * NavLength,Color.red);
+
+        /*
+        if (Physics.Raycast(N, out HitN, NavLength)){    
+            if(Hit.collider.tag != "Player" && Hit.collider.tag != "Enemy"){    }
+        }
+        if (Physics.Raycast(NE, out HitNE, NavLength)){    
+            if(Hit.collider.tag != "Player" && Hit.collider.tag != "Enemy"){    }
+        }
+        if (Physics.Raycast(E, out HitE, NavLength)){    
+            if(Hit.collider.tag != "Player" && Hit.collider.tag != "Enemy"){    }
+        }
+        if (Physics.Raycast(SE, out HitSE, NavLength)){    
+            if(Hit.collider.tag != "Player" && Hit.collider.tag != "Enemy"){    }
+        }
+        if (Physics.Raycast(S, out HitS, NavLength)){    
+            if(Hit.collider.tag != "Player" && Hit.collider.tag != "Enemy"){    }
+        }
+        if (Physics.Raycast(SW, out HitSW, NavLength)){    
+            if(Hit.collider.tag != "Player" && Hit.collider.tag != "Enemy"){    }
+        }
+        if (Physics.Raycast(W, out HitW, NavLength)){    
+            if(Hit.collider.tag != "Player" && Hit.collider.tag != "Enemy"){    }
+        }
+        if (Physics.Raycast(NW, out HitNW, NavLength)){    
+            if(Hit.collider.tag != "Player" && Hit.collider.tag != "Enemy"){    }
+        }
+        */
+    }
     void Follow(){
+        //Change pos to cardinal points
         RB.MovePosition(Vector3.MoveTowards(transform.position, Player.transform.position, MoveSpeed/50));
         Target = new Vector3 (Player.transform.position.x, transform.position.y, Player.transform.position.z);
         TargetDir = Quaternion.LookRotation(Target - transform.position);
@@ -86,32 +143,17 @@ public class AI_Navigation : MonoBehaviour
             return Random.Range(-130, 130);
         return Random.Range(-180, 180);
     }
-    IEnumerator NewDirection(){
+    void NewDirection(){
         CurrentDir = transform.rotation;
         IdelDir = CurrentDir * Quaternion.Euler(0,TurnBias(),0);
-        yield return new WaitForSeconds(IdelPathTime);
-        IdelRoutine = NewDirection(); StartCoroutine(IdelRoutine);
     }
-    IEnumerator Lingering(){
-        yield return new WaitForSeconds(IdelPathTime * IdelPathTime);
+    void Lingering(){
         if (!Sighting && !Idel) Idel = true;
-        AlertRoutine = Lingering(); StartCoroutine(AlertRoutine);
     }
-    IEnumerator RandomDetect(){
+    void RandomDetect(){
         float i = 0;
         PlayerDistance = Vector3.Distance(Player.transform.position, transform.position);
-        i = Random.Range(0f, (float)PlayerDistance/Alertness); if (i < 0.3f) Idel = false;
-        yield return new WaitForSeconds((float)IdelPathTime/2);
-        RandomRoutine = RandomDetect(); StartCoroutine(RandomRoutine);
+        i = Random.Range(0f, (float)PlayerDistance/Alertness); 
+        if (i < 0.3f) Idel = false;
     }
 }
-
-/*      NOTE SECTION
-
-Okay, use 4 or 8 rays at base of A.I; if forward ray is hitting something that is not the player. Then the 
-A.I should start moving in the direction of ray that is both not hitting something and whose length
-is closest to player.
-
-Maybe alertness should rely on dedicated ray?
-
-*/
